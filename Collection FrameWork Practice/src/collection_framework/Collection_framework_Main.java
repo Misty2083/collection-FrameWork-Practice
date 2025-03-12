202b09971154a16308b9845f193696a26adc1fd3
Mymuna misty <161964733+Misty2083@users.noreply.github.com>
2025-03-12 11:16:43 +0600

    Initial commit

---
+++ Collection FrameWork Practice/src/collection_framework/Collection_framework_Main.java
@@ -0,0 +1,53 @@
+package collection_framework;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Collection_framework_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Collection<String> c=new ArrayList<String>();
+c.add("Apple");
+c.add("Orange");
+c.add("JackFruit");
+c.add("Berry");
+
+for(String i:c)
+	System.out.println(i);
+
+System.out.println(c);*/
+		
+		/*List c=new ArrayList();
+		c.add("Apple");
+		c.add("Orange");
+		c.add("JackFruit");
+		c.add("Berry");
+		
+		for(int i=0;i<c.size();i++)
+			System.out.println(c.get(i));*/
+		
+//		LinkedList<String> c=new LinkedList<String>();
+//		c.add("Apple");
+//		c.add("Orange");
+//		c.add("JackFruit");
+//		c.add("Berry");
+//		
+//		for(int i=0;i<c.size();i++)
+//			System.out.println(c.get(i));
+//		
+//		LinkedList<String> list= new LinkedList<String>();
+//		list.add(0,"A");
+//		list.add(1,"B");
+//		list.addAll(c);
+//		
+//     	for(int i=0;i<list.size();i++) {
+//		System.out.println(list.get(i));
+//		System.out.println(list.contains("A"));
+//		}
+//		
+	}
+
+}
